0746498e5cec7a835f25900b350962dbcbe22222
Bogdan Druzynin <bogdandruzynin@Bogdans-MacBook-Pro.local>
2020-01-30 14:23:39 +0200

    Updates

---
+++ precache-manifest.900130a309180fa122c4ea5612f69329.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1282d84191e54e61833bd685cec9b5f6",
+    "url": "/gog-shop/index.html"
+  },
+  {
+    "revision": "5d9a29baa99d4d95ed78",
+    "url": "/gog-shop/static/css/2.20551998.chunk.css"
+  },
+  {
+    "revision": "6198acee09c3e9bf8b00",
+    "url": "/gog-shop/static/css/main.5d9fc38a.chunk.css"
+  },
+  {
+    "revision": "5d9a29baa99d4d95ed78",
+    "url": "/gog-shop/static/js/2.c16437bc.chunk.js"
+  },
+  {
+    "revision": "6198acee09c3e9bf8b00",
+    "url": "/gog-shop/static/js/main.315c9971.chunk.js"
+  },
+  {
+    "revision": "7714be8e8194fa483d82",
+    "url": "/gog-shop/static/js/runtime~main.b8ee74fb.js"
+  },
+  {
+    "revision": "98e7e7b8114c751b1c945168971a5992",
+    "url": "/gog-shop/static/media/assasins_creed.98e7e7b8.png"
+  },
+  {
+    "revision": "1a35ac0c8b2b420322818ddc08329a1f",
+    "url": "/gog-shop/static/media/deponia.1a35ac0c.png"
+  },
+  {
+    "revision": "ee3275ed477f97e16cf3d980941c01fe",
+    "url": "/gog-shop/static/media/neverwinter.ee3275ed.png"
+  },
+  {
+    "revision": "cb2a43a927b07bc691202997ea63492d",
+    "url": "/gog-shop/static/media/oddworld.cb2a43a9.png"
+  },
+  {
+    "revision": "c8e341c4c2cc00ee7adbf59c3a13b15c",
+    "url": "/gog-shop/static/media/settlers.c8e341c4.png"
+  },
+  {
+    "revision": "0fc1d28b96cc661f438b882313ce3106",
+    "url": "/gog-shop/static/media/witcher_cover.0fc1d28b.png"
+  }
+]);
